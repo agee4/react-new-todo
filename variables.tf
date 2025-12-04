d4bc5f08001a08050282296fc5941862ad9b9b0b
Aaron Gee <116915671+agee4@users.noreply.github.com>
2025-12-04 23:15:44 +0000

    Add AWS access and secret key variables

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+  description = "access key for aws"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "secret key for aws"
+  type        = string
+}
