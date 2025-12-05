9d41e702d6610aba70f93a1af1a8f37dc1a90932
Aaron Gee <116915671+agee4@users.noreply.github.com>
2025-12-05 00:22:02 +0000

    Update PersonList component layout and styling
    Refactor PersonList component to use a styled container and list items.

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import axios from 'axios';
-
+import './PersonList.css';
+ 
 export default class PersonList extends React.Component {
   state = {
     persons: []
   }
-
+ 
   componentDidMount() {
     axios.get(`https://a240oa1749.execute-api.us-west-2.amazonaws.com/prod/get-todo`)
       .then(res => {
@@ -13,17 +14,21 @@ export default class PersonList extends React.Component {
         this.setState({ persons });
       })
   }
-
+ 
   render() {
     return (
-      <ul>
-        {
-          this.state.persons
-            .map(person =>
-              <li key={person.id}>{person.id}{person.name}</li>
-            )
-        }
-      </ul>
+      <div className="person-list-container">
+        <h1 className="title">Person List</h1>
+        <ul className="person-list">
+          {
+            this.state.persons.map(person => (
+              <li key={person.id} className="person-item">
+                {person.id} - {person.name}
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     )
   }
 }
